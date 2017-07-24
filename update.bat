d85daeb88e630f232edaa68e9eac019ef0c1e6fb
hcai <hcai@eecs.wsu.edu>
2017-07-24 13:15:52 -0700

    updated webpage after ICSME notifications

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-C:\"Program Files (x86)"\Bibtex2html\bibtex2html.exe -heveaurl -noheader -nokeys -style "plain" -html-entities  -dl -unicode -r -d  -nofooter  -t "Publications" hcaipub.bib
+C:\"Program Files (x86)"\Bibtex2html\bibtex2html.exe -heveaurl -noheader -nokeys -style "plain" -html-entities  -dl -unicode -r -d -nofooter  -t "Publications" hcaipub.bib
