3f37b8fe257a56a32df150857718ae7c9cf2f5bc
haipeng_cai <chapering@gmail.com>
2019-10-23 19:49:07 -0700

    student info update

--- update.bat
+++ update.bat
@@ -1,1 +1,3 @@
 C:\"Program Files (x86)"\Bibtex2html\bibtex2html.exe -heveaurl -noheader -nokeys -style "plain" -html-entities  -dl -unicode -r -d -nofooter  -t "Publications" hcaipub.bib
+
+timeout 5
